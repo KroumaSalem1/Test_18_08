9d18d280da5215ab7b89d6847f8c2ea1d7922721
NejmiRiseUp <nejmi.zagoub@riseup.com.tn>
2017-08-18 12:07:23 +0200

    Add controller with action

---
+++ DXApplication1/XafApplication1.Module/Controllers/NejmiController.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace XafApplication1.Module.Controllers
+{
+    partial class NejmiController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleAction1 = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // simpleAction1
+            // 
+            this.simpleAction1.Caption = "doTest";
+            this.simpleAction1.ConfirmationMessage = null;
+            this.simpleAction1.Id = "btn_action";
+            this.simpleAction1.ToolTip = null;
+            // 
+            // NejmiController
+            // 
+            this.Actions.Add(this.simpleAction1);
+            this.TargetObjectType = typeof(XafApplication1.Module.BusinessObjects.Nejmi);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleAction1;
+    }
+}
